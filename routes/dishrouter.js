a533904d980d37b636c101b25fbd079b962df90d
Rahul Bhat <bhatrahul801@gmail.com>
2021-09-05 20:42:22 +0530

    new up

--- routes/dishrouter.js
+++ routes/dishrouter.js
@@ -1,4 +1,3 @@
-// Assignment 1
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -43,5 +42,41 @@ dishRouter.route('/:dishId')
     .delete(function (req, res, next) {
         res.end('Deleting dish: ' + req.params.dishId);
     });
+    dishRouter.route('/:dishId/comments')
+    .all(function (req, res, next) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        next();
+    })
 
+    .get(function (req, res, next) {
+        res.end('Will send all the dishes to you!');
+    })
+
+    .post(function (req, res, next) {
+        res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+    })
+
+    .delete(function (req, res, next) {
+        res.end('Deleting all dishes');
+    });
+
+dishRouter.route('/:dishId/comments/:commentsId')
+    .all(function (req, res, next) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        next();
+    })
+
+    .get(function (req, res, next) {
+        res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+    })
+
+    .put(function (req, res, next) {
+        res.write('Updating the dish: ' + req.params.dishId + '\n');
+        res.end('Will update the dish: ' + req.body.name +
+            ' with details: ' + req.body.description);
+    })
+
+    .delete(function (req, res, next) {
+        res.end('Deleting dish: ' + req.params.dishId);
+    });
 exports.router = dishRouter;
